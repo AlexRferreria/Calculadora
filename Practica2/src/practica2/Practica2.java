e7199d36a61f70ed78001ab1c3376ade530639e5
adriv <adriv@DESKTOP-EPHGIAH>
2022-04-07 19:09:36 +0200

    Cambios de los formatos de salida

--- Practica2/src/practica2/Practica2.java
+++ Practica2/src/practica2/Practica2.java
@@ -26,17 +26,18 @@ public class Practica2 {
 
             String numero1;
             do {
-                System.out.println("\n Introdueix el primer numero. ");
+                System.out.println("\nIntrodueix el primer numero. ");
                 numero1 = sc.nextLine();
             } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume1 = Double.parseDouble(numero1);
             double n1 = new Double(numero1);
 
             do {
-                System.out.println("\n Operació? (Indica el signe)");
-                System.out.println("+ = sumar \n - = restar \n"
-                        + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
-                        + "\n % = residu");
+                System.out.println("\nOperació? (Indica el signe)");
+                System.out.println("+ = sumar \n" + "- = restar \n"
+                        + "x = multiplicar \n" + "/ = dividir \n" 
+                        + "* = elevar primer num al segon num. \n"
+                        + "% = residu");
                 operacion = sc.nextLine();
                 if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
                         || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
@@ -49,7 +50,7 @@ public class Practica2 {
 
             String numero2;
             do {
-                System.out.println("\n Introdueix el segon numero.");
+                System.out.println("\nIntrodueix el segon numero.");
                 numero2 = sc.nextLine();
             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume2 = Double.parseDouble(numero2);
@@ -71,8 +72,8 @@ public class Practica2 {
                     case "/":
                         while (n2 == 0) {
                             do {
-                                System.err.println(" Al denominador hi ha un zero \n"
-                                        + "per a  evitar errors coloca un altre valor.");
+                                System.err.println("Al denominador hi ha un zero \n"
+                                        + "per a evitar errors coloca un altre valor.");
                                 numero2 = sc.nextLine();
                             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
                             nume2 = Double.parseDouble(numero2);
@@ -86,8 +87,8 @@ public class Practica2 {
                     case "%":
                         while (n2 == 0) {
                             do {
-                                System.err.println(" Al denominador hi ha un zero \n"
-                                        + "per a  evitar errors coloca un altre valor.");
+                                System.err.println("Al denominador hi ha un zero \n"
+                                        + "per a evitar errors coloca un altre valor.");
                                 numero2 = sc.nextLine();
                             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
                             nume2 = Double.parseDouble(numero2);
@@ -98,9 +99,9 @@ public class Practica2 {
                 }
             } while (comprobar != true);
 
-            System.out.println("(" + numero1 + ") " + operacion + " (" + numero2 + ")" + " = " + res);
-            System.out.println("\n Vols continuar operant? \n");
-            System.out.println(" [s/n]");
+            System.out.println(numero1 + " " + operacion + " " + numero2 + " = " + res);
+            System.out.println("\nVols continuar operant? \n");
+            System.out.println("[s/n]");
             do {
                 comprobar = true;
                 operacion = sc.nextLine();
@@ -112,7 +113,7 @@ public class Practica2 {
                     case "N":
                         break;
                     default:
-                        System.err.println("\n Error, posa un valor vàlid. \n");
+                        System.err.println("\nError, posa un valor vàlid. \n");
                         comprobar = false;
                 }
             } while (comprobar != true);
